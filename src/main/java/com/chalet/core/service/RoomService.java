1d3a99addcecf137460bfd6e16241b8e588ed283
Aaradhita Behera <manjeetkumar@aaruchalet.com>
2026-09-17 19:31:01 +0530

    feat: define room service contract

--- src/main/java/com/chalet/core/service/RoomService.java
+++ src/main/java/com/chalet/core/service/RoomService.java
@@ -1,4 +1,18 @@
 package com.chalet.core.service;
 
+import com.chalet.core.dto.request.RoomRequest;
+import com.chalet.core.dto.response.RoomResponse;
+import java.util.List;
+
 public interface RoomService {
+
+  List<RoomResponse> findAll();
+
+  RoomResponse findById(Long id);
+
+  RoomResponse create(RoomRequest request);
+
+  RoomResponse update(Long id, RoomRequest request);
+
+  void delete(Long id);
 }
