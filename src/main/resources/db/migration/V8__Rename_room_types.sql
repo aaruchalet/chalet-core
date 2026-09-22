650fd777204835a171c999847018e176e92232a4
Aaradhita Behera <manjeetkumar@aaruchalet.com>
2026-09-23 01:58:12 +0530

    feat(db): rename room types for Aaru Chalet

---
+++ src/main/resources/db/migration/V8__Rename_room_types.sql
@@ -0,0 +1,3 @@
+UPDATE room_type SET type_name = 'Pine Haven' WHERE id = 1;
+UPDATE room_type SET type_name = 'Cedar Retreat' WHERE id = 2;
+UPDATE room_type SET type_name = 'Himalayan Panorama' WHERE id = 3;
